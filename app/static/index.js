7013728ba8c581a7e6c9d97f279e0fbb93a0e7c6
Vadim Ovcearenco <wadim@veles-soft.com>
2020-08-12 12:14:47 +0000

    Show questions and answers

--- app/static/index.js
+++ app/static/index.js
@@ -3,14 +3,63 @@
 const e = React.createElement;
 
 const mockSurvey = {
-    title: "This is my mock survey"
+    title: "This is my mock survey",
+    questions: [
+        {
+            id: 1,
+            title: "Single choice",
+            type: "single",
+            answers: [
+                {id: 1, title: "Answer1"},
+                {id: 2, title: "Answer2"},
+                {id: 3, title: "Answer3"},
+            ]
+        }
+    ]
 };
 
 
+function QSingle(props) {
+}
+
+
+function Answer(props) {
+    const onChange = function (e) {
+        props.question.selected = e.target.value;
+    }
+    return (
+        <div>
+            <input type="radio" name={props.question.id} value={props.answer.id} onChange={ onChange }/> { props.answer.title }
+        </div>
+    )
+
+}
+
+function Question(props) {
+    const answerItems = props.question.answers.map(answer => {
+        return (<Answer key= {answer.id} answer={ answer} question={props.question}/>)
+    })
+    return (
+        <div>
+        <h3>{ props.question.title}</h3>
+        { answerItems }
+        </div>
+    )
+}
+
 function Survey(props) {
     const survey = props.survey;
+    const questionItems = survey.questions.map((q) => <Question key={ q.id } question={q} />);
+    const saveResult = function () {
+        alert("Save");
+        console.log(survey);
+    }
     return (
+        <div>
         <h2>{ survey.title }</h2>
+        { questionItems }
+        <button onClick={ saveResult }>Save</button>
+        </div>
     )
 
 }
